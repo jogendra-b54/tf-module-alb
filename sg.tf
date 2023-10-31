f05e757c39c024c64dc7b10cabbf7c8560e99e30
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-01 00:12:34 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -1,5 +1,7 @@
 # Public Load Balancer Security Group
 resource "aws_security_group" "alb_public" {
+
+  count =   var.INTERNAL ? 0 : 1
   name        = "roboshop-${var.ENV}-public-alb-sg"
   description = "Allows Traffic From Internet"
   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
@@ -29,6 +31,7 @@ resource "aws_security_group" "alb_public" {
 
 # Public Load Balancer Security Group
 resource "aws_security_group" "alb_private" {
+  count =   var.INTERNAL ? 0 : 1
   name        = "roboshop-${var.ENV}-private-alb-sg"
   description = "Allows Traffic From Intranet Only"
   vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
