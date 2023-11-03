152f45788e8078f87d072eff271f7e25d060de2c
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-03 19:22:25 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "ALB_ARN" {
     value = aws_lb.robot_alb.arn
 
 }
+
+output "LISTENER_ARN" {
+    
+    value = aws_lb_listener.private.*.arn // * is coming to picutr becoz we are using a count , i am only taking the 1st value , if it is 0 thereis no point and if it is 1 then i need to take and capture it
+
+} 
