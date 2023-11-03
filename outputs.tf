55847d3f59c3d59d6c602ecb59a20963cc966175
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-03 20:12:37 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -8,4 +8,9 @@ output "LISTENER_ARN" {
     
     value = aws_lb_listener.private.*.arn // * is coming to picutr becoz we are using a count , i am only taking the 1st value , if it is 0 thereis no point and if it is 1 then i need to take and capture it
 
-} 
+}
+
+
+output "ALB_ADDRESS" {
+  value = aws_lb.robot_alb.dns_name
+}
